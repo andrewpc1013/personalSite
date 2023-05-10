b58434f99fe155aa957caf7b0271bfd68eb7d6f7
Andrew Conlin <andrewpc1013@gmail.com>
2023-05-10 11:31:45 -0500

    changed style of whole page, edited description

--- src/components/Description.jsx
+++ src/components/Description.jsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 const Description = () => {
-  return <div>Description</div>;
+  return (
+    <div className="bg-gray-200">
+      <h1>I'm Andrew Conlin</h1>
+      <h2>I am a fullstack developer and former mechanical engineer.</h2>
+    </div>
+  );
 };
 
 export default Description;
