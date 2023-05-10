3458f575abb4e2eaaaefafbe88143618f7bde96c
Andrew Conlin <andrewpc1013@gmail.com>
2023-05-10 12:02:19 -0500

    added vertical timeline

--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -1,7 +1,65 @@
 import React from "react";
+import {
+  VerticalTimeline,
+  VerticalTimelineElement,
+} from "react-vertical-timeline-component";
+import "react-vertical-timeline-component/style.min.css";
 
 const Timeline = () => {
-  return <div>Timeline</div>;
+  return (
+    <div>
+      <VerticalTimeline>
+        <VerticalTimelineElement>
+          <h1>
+            timeline1 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Nibh sit amet commodo nulla facilisi nullam vehicula. Vulputate
+            sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum.
+            Enim tortor at auctor urna nunc id cursus. Nam at lectus urna duis
+            convallis convallis tellus id. Massa placerat duis ultricies lacus
+            sed. Eleifend mi in nulla posuere sollicitudin aliquam ultrices
+            sagittis. Turpis nunc eget lorem dolor sed viverra. Praesent
+            tristique magna sit amet purus gravida. Mattis molestie a iaculis at
+            erat pellentesque. Fermentum odio eu feugiat pretium nibh ipsum.
+            Dictum varius duis at consectetur. Tristique senectus et netus et
+            malesuada fames.
+          </h1>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement>
+          <h1>
+            timeline2 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Nibh sit amet commodo nulla facilisi nullam vehicula. Vulputate
+            sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum.
+            Enim tortor at auctor urna nunc id cursus. Nam at lectus urna duis
+            convallis convallis tellus id. Massa placerat duis ultricies lacus
+            sed. Eleifend mi in nulla posuere sollicitudin aliquam ultrices
+            sagittis. Turpis nunc eget lorem dolor sed viverra. Praesent
+            tristique magna sit amet purus gravida. Mattis molestie a iaculis at
+            erat pellentesque. Fermentum odio eu feugiat pretium nibh ipsum.
+            Dictum varius duis at consectetur. Tristique senectus et netus et
+            malesuada fames.
+          </h1>
+        </VerticalTimelineElement>
+        <VerticalTimelineElement>
+          <h1>
+            timeline3 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
+            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+            Nibh sit amet commodo nulla facilisi nullam vehicula. Vulputate
+            sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum.
+            Enim tortor at auctor urna nunc id cursus. Nam at lectus urna duis
+            convallis convallis tellus id. Massa placerat duis ultricies lacus
+            sed. Eleifend mi in nulla posuere sollicitudin aliquam ultrices
+            sagittis. Turpis nunc eget lorem dolor sed viverra. Praesent
+            tristique magna sit amet purus gravida. Mattis molestie a iaculis at
+            erat pellentesque. Fermentum odio eu feugiat pretium nibh ipsum.
+            Dictum varius duis at consectetur. Tristique senectus et netus et
+            malesuada fames.
+          </h1>
+        </VerticalTimelineElement>
+      </VerticalTimeline>
+    </div>
+  );
 };
 
 export default Timeline;
