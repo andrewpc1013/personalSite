adc793eab296b0f39b799fe341abaabdcbae4e7f
Andrew Conlin <andrewpc1013@gmail.com>
2023-05-18 11:48:23 -0500

    added timeline content

---
+++ src/assets/index.js
@@ -0,0 +1,5 @@
+import FSLogo from "./FSLogo.png";
+import Infinity from "./Infinity.png";
+import ULLogo from "./UL.jpeg"
+
+export { FSLogo, Infinity, ULLogo };
--- src/components/Timeline.jsx
+++ src/components/Timeline.jsx
@@ -4,58 +4,83 @@ import {
   VerticalTimelineElement,
 } from "react-vertical-timeline-component";
 import "react-vertical-timeline-component/style.min.css";
+import { FSLogo, Infinity, ULLogo } from "../assets";
 
 const Timeline = () => {
   return (
     <div>
       <VerticalTimeline>
-        <VerticalTimelineElement>
-          <h1>
-            timeline1 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Nibh sit amet commodo nulla facilisi nullam vehicula. Vulputate
-            sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum.
-            Enim tortor at auctor urna nunc id cursus. Nam at lectus urna duis
-            convallis convallis tellus id. Massa placerat duis ultricies lacus
-            sed. Eleifend mi in nulla posuere sollicitudin aliquam ultrices
-            sagittis. Turpis nunc eget lorem dolor sed viverra. Praesent
-            tristique magna sit amet purus gravida. Mattis molestie a iaculis at
-            erat pellentesque. Fermentum odio eu feugiat pretium nibh ipsum.
-            Dictum varius duis at consectetur. Tristique senectus et netus et
-            malesuada fames.
-          </h1>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "gray", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="August 2014 - May 2018"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<img src={ULLogo} alt="ULLogo" />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Bachelor's Degree in Mechanical Engineering
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            University of Louisiana at Lafayette
+          </h4>
+          <ul>
+            <li>
+              Worked as an undergraduate research assistant in two robotics labs
+              as a programmer and in one materials science lab.
+            </li>
+            <li>Took classes using Python, Arduino C, and Matlab.</li>
+          </ul>
         </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          <h1>
-            timeline2 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Nibh sit amet commodo nulla facilisi nullam vehicula. Vulputate
-            sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum.
-            Enim tortor at auctor urna nunc id cursus. Nam at lectus urna duis
-            convallis convallis tellus id. Massa placerat duis ultricies lacus
-            sed. Eleifend mi in nulla posuere sollicitudin aliquam ultrices
-            sagittis. Turpis nunc eget lorem dolor sed viverra. Praesent
-            tristique magna sit amet purus gravida. Mattis molestie a iaculis at
-            erat pellentesque. Fermentum odio eu feugiat pretium nibh ipsum.
-            Dictum varius duis at consectetur. Tristique senectus et netus et
-            malesuada fames.
-          </h1>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "gray", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="February 2019 - November 2022"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<img src={Infinity} alt="Infinity" />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Mechanical Engineer
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Infinity Engineering Consultants
+          </h4>
+          <ul>
+            <li>
+              Developed construction drawing sets under supervision of
+              professional engineers.
+            </li>
+            <li>
+              Managed projects including client communication, team management,
+              budget management
+            </li>
+          </ul>
         </VerticalTimelineElement>
-        <VerticalTimelineElement>
-          <h1>
-            timeline3 Lorem ipsum dolor sit amet, consectetur adipiscing elit,
-            sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
-            Nibh sit amet commodo nulla facilisi nullam vehicula. Vulputate
-            sapien nec sagittis aliquam malesuada bibendum arcu vitae elementum.
-            Enim tortor at auctor urna nunc id cursus. Nam at lectus urna duis
-            convallis convallis tellus id. Massa placerat duis ultricies lacus
-            sed. Eleifend mi in nulla posuere sollicitudin aliquam ultrices
-            sagittis. Turpis nunc eget lorem dolor sed viverra. Praesent
-            tristique magna sit amet purus gravida. Mattis molestie a iaculis at
-            erat pellentesque. Fermentum odio eu feugiat pretium nibh ipsum.
-            Dictum varius duis at consectetur. Tristique senectus et netus et
-            malesuada fames.
-          </h1>
+        <VerticalTimelineElement
+          className="vertical-timeline-element--work"
+          contentStyle={{ background: "gray", color: "#fff" }}
+          contentArrowStyle={{ borderRight: "7px solid  rgb(33, 150, 243)" }}
+          date="January 2023 - April 2023"
+          iconStyle={{ background: "rgb(33, 150, 243)", color: "#fff" }}
+          icon={<img src={FSLogo} alt="FSLogo" />}
+        >
+          <h3 className="vertical-timeline-element-title">
+            Web Development Student
+          </h3>
+          <h4 className="vertical-timeline-element-subtitle">
+            Fullstack Academy
+          </h4>
+          <ul>
+            <li>
+              Developed front-end web apps using html, css, javascript, and
+              react
+            </li>
+            <li>
+              Developed back-end servers and databases using postgresql and
+              javascript
+            </li>
+          </ul>
         </VerticalTimelineElement>
       </VerticalTimeline>
     </div>
