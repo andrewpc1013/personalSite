f9f4c82cefc9d30cbfdd108d9e2cf6e24822b3ae
Andrew Conlin <andrewpc1013@gmail.com>
2023-05-23 15:26:25 -0500

    added project cards

--- src/assets/index.js
+++ src/assets/index.js
@@ -1,5 +1,8 @@
 import FSLogo from "./FSLogo.png";
 import Infinity from "./Infinity.png";
-import ULLogo from "./UL.jpeg"
+import ULLogo from "./UL.jpeg";
+import ebazon from "./ebazon.png";
+import FitnessTracker from "./FitnessTracker.png";
+import StrangersThings from "./StrangersThings.png";
 
-export { FSLogo, Infinity, ULLogo };
+export { FSLogo, Infinity, ULLogo, ebazon, FitnessTracker, StrangersThings };
